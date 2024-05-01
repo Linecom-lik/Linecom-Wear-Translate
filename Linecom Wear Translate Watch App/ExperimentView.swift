1cf5619d4a311d52a1dd3fe9dab3dd6a83e7f501
Linecom-lik <3448336734@qq.com>
2024-05-01 22:30:29 +0800

    1.0.7 CU 01

---
+++ Linecom Wear Translate Watch App/ExperimentView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperimentView.swift
+//  Linecom Wear Translate Watch App
+//
+//  Created by 澪空 on 2024/5/1.
+//
+
+import SwiftUI
+
+struct ExperimentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperimentView()
+}
