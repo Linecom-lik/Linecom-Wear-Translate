3381914d626a56d46974bb1a45d039c93dac470b
Linecom-lik <3448336734@qq.com>
2024-05-12 14:15:37 +0800

    Delete Test

---
+++ Linecom Wear Translate Watch App/UpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateView.swift
+//  Linecom Wear Translate Watch App
+//
+//  Created by 澪空 on 2024/5/12.
+//
+
+import SwiftUI
+
+struct UpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateView()
+}
