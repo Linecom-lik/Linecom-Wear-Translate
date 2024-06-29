8c311f81fc0170e4bc2f8bbce93a78cc4831be82
Linecom-lik <3448336734@qq.com>
2024-06-29 17:06:54 +0800

    优化文言翻译

--- Linecom Wear Translate Watch App/WYWTranslate.swift
+++ Linecom Wear Translate Watch App/WYWTranslate.swift
@@ -9,34 +9,62 @@ import SwiftUI
 import DarockKit
 
 struct WYWTranslate: View {
-    @State var wywin=""
-    @State var wywout=""
-    @State var req=false
-    var body: some View {
-        List{
-            Section{
-                TextField("输入文言", text: $wywin)
-                Button(action: {
-                    req=true
-                    DarockKit.Network.shared.requestJSON("https://api.linecom.net.cn/lwt/translate?provider=baidu&text=\(wywin)&slang=wyw&tlang=zh&pass=l1nec0m".urlEncoded()){
-                        resp, successd in
-                        wywout=resp["trans_result"][0]["dst"].string ?? ""
-                        req=false
-                    }
-                }, label: {
-                    if !req{
-                        Text("翻译")
-                    } else {
-                        ProgressView()
-                    }
-                })
-            }
-            if !wywout.isEmpty{
+    var body: some View{
+        TabView{
+            WYWToCN()
+        }
+    }
+    
+    struct WYWToCN: View {
+        @State var wywin=""
+        @State var wywout=""
+        @State var req=false
+        var body: some View {
+            List{
                 Section{
-                    Text(wywout)
+                    HStack{
+                        Spacer()
+                        Text("文言->中文")
+                        Spacer()
+                    }
                 }
+                Section{
+                    TextField("输入文言", text: $wywin)
+                    Button(action: {
+                        req=true
+                        if !wywin.isEmpty{
+                            DarockKit.Network.shared.requestJSON("https://api.linecom.net.cn/lwt/translate?provider=baidu&text=\(wywin)&slang=wyw&tlang=zh&pass=l1nec0m".urlEncoded()){
+                                resp, successd in
+                                wywout=resp["trans_result"][0]["dst"].string ?? "返回错误"
+                            }
+                        } else {
+                            wywout="请输入文本"
+                        }
+                        req=false
+                    }, label: {
+                        if !req{
+                            HStack{
+                                Spacer()
+                                Text("翻译")
+                                Spacer()
+                            }
+                        } else {
+                            ProgressView()
+                        }
+                    })
+                }
+                if !wywout.isEmpty{
+                    Section{
+                        Text(wywout)
+                        Button(action: {
+                            wywin=""
+                        }, label: {
+                            Text("重置")
+                        })
+                    }
+                }
+                
             }
-            
         }
     }
 }
